2bb9165cd8c7d4235d76655b9e8d915bc6d974c0
czx <760027413@qq.com>
2018-03-14 15:50:46 +0800

    egerge

---
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,38 @@
+package cn.e3mall.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.common.pojo.DataGridResult;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+/**
+ * 商品管理Controller
+ * @author Administrator
+ *
+ */
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemByID(@PathVariable long itemId){
+		TbItem item = itemService.getItemById(itemId);
+		return item;
+	}
+	
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public DataGridResult getItemList(int page,int rows){
+		DataGridResult result = itemService.getItemListDataGrid(page, rows);
+		return result;
+	}
+	
+}
